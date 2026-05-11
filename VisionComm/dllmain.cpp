5479d7b5a2bb1f5b64f56ee2007e5c9aa7cc0d76
bongsoo.kim <bongsoo.kim@techwing.co.kr>
2026-05-11 11:30:04 +0900

    54254325432

--- VisionComm/dllmain.cpp
+++ VisionComm/dllmain.cpp
@@ -1,4 +1,4 @@
-﻿// dllmain.cpp : DLL의 초기화 루틴을 정의합니다.
+// dllmain.cpp : DLL의 초기화 루틴을 정의합니다.
 //
 
 #include "stdafx.h"
@@ -9,7 +9,7 @@
 #define new DEBUG_NEW
 #endif
 
-static AFX_EXTENSION_MODULE VisionComCoreDLL = { NULL, NULL };
+static AFX_EXTENSION_MODULE VisionCommDLL = { NULL, NULL };
 
 extern "C" int APIENTRY
 DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
@@ -19,10 +19,10 @@ DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 
 	if (dwReason == DLL_PROCESS_ATTACH)
 	{
-		TRACE0("VisionComCore.DLL을 초기화하고 있습니다.\n");
+		TRACE0("VisionComm.DLL을 초기화하고 있습니다.\n");
 		
 		// 확장 DLL을 한 번만 초기화합니다.
-		if (!AfxInitExtensionModule(VisionComCoreDLL, hInstance))
+		if (!AfxInitExtensionModule(VisionCommDLL, hInstance))
 			return 0;
 
 		// 이 DLL을 리소스 체인에 삽입합니다.
@@ -37,15 +37,15 @@ DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 		//  기본 DLL의 리소스 체인에 추가되지 않으므로
 		//  심각한 문제가 발생합니다.
 
-		new CDynLinkLibrary(VisionComCoreDLL);
+		new CDynLinkLibrary(VisionCommDLL);
 
 	}
 	else if (dwReason == DLL_PROCESS_DETACH)
 	{
-		TRACE0("VisionComCore.DLL을 종료하고 있습니다.\n");
+		TRACE0("VisionComm.DLL을 종료하고 있습니다.\n");
 
 		// 소멸자가 호출되기 전에 라이브러리를 종료합니다.
-		AfxTermExtensionModule(VisionComCoreDLL);
+		AfxTermExtensionModule(VisionCommDLL);
 	}
 	return 1;   // 확인
 }
