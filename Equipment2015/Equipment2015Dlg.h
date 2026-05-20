c4267cfd04cb1123e4b5474b9165a150580c6811
bongsoo.kim <bongsoo.kim@techwing.co.kr>
2026-05-20 17:41:09 +0900

    58jgfkldsg;

--- Equipment2015/Equipment2015Dlg.h
+++ Equipment2015/Equipment2015Dlg.h
@@ -9,27 +9,24 @@
 // CEquipment2015Dlg 대화 상자
 class CEquipment2015Dlg : public CDialogEx
 {
- VMF::CVatEngineObserverAdapter m_engine;
- std::shared_ptr<VMF_Load1::Load1Parts> m_parts;
- std::shared_ptr<VMF_Load1::VatAdapterLoad1> m_adapter;
+    VMF::CVatEngineObserverAdapter m_engine;
+    std::shared_ptr<Load1Parts> m_parts;
+    std::shared_ptr<VMF_Load1::VatAdapterLoad1> m_adapter;
 
- // Observer subscription id (for safe unsubscribe when dialog is destroyed)
- VMF::CVatEngineObserverAdapter::ObserverId m_engineObserverId;
+    // Observer subscription id (for safe unsubscribe when dialog is destroyed)
+    VMF::CVatEngineObserverAdapter::ObserverId m_engineObserverId;
 
-// 생성입니다.
 public:
 	CEquipment2015Dlg(CWnd* pParent = NULL);	// 표준 생성자입니다.
 
 // 대화 상자 데이터입니다.
 #ifdef AFX_DESIGN_TIME
-	enum { IDD = IDD_EQUIPMENT2015_DIALOG };
+    enum { IDD = IDD_EQUIPMENT2015_DIALOG };
 #endif
 
-	protected:
+protected:
 	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
 
-
-// 구현입니다.
 protected:
 	HICON m_hIcon;
 
@@ -42,5 +39,5 @@ protected:
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
 public:
- afx_msg void OnBnClickedVisionSequence();
+    afx_msg void OnBnClickedVisionSequence();
 };
