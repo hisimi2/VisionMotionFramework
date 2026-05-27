71977f37eed53f21d9746fe7c7879981fa2090e7
bongsoo.kim <bongsoo.kim@techwing.co.kr>
2026-05-27 17:14:47 +0900

    4432143214321

--- bin/include/VisionCommAPI.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef VISION_COMM_EXPORTS
-#define VISION_COMM_API __declspec(dllexport)
-#else
-#define VISION_COMM_API __declspec(dllimport)
-#endif\n// Backwards-compatibility: alias old namespace name to new one\nnamespace VC {}\nnamespace VisionCom = VisionComm;\n
