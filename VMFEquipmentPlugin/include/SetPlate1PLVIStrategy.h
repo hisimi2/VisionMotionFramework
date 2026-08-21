29ad358cf6b2a626623fc9c8e57ebb09afd00be8
bongsoo.kim <bongsoo.kim@techwing.co.kr>
2026-08-21 18:04:57 +0900

    1차 리팩토링 완료.

--- VMFEquipmentPlugin/include/SetPlate1PLVIStrategy.h
+++ VMFEquipmentPlugin/include/SetPlate1PLVIStrategy.h
@@ -10,12 +10,6 @@ namespace VMF_PLUGIN
 {
     class SetPlate1PLVIStrategy : public VMF::DefaultSetupStrategy
     {
-    public:
-        std::string GetSequenceName() const override;
-        VMF::DataRepositoryPtr CreateRepository() override;
-        VMF::VisionProcessorPtr CreateVisionProcessor() override;
-        VMF::SequenceBuilderPtr CreateBuilder() override;
-        void ConfigureContext(VMF::Context& ctx) override;
         VMF::TaskParams GetDefaultParams() const;
         VMF::TaskParams GetSetupParams() const;
         VMF::TaskParams GetExecuteScanParams() const;
@@ -23,6 +17,14 @@ namespace VMF_PLUGIN
         VMF::TaskParams GetVisionParams() const;
         void SetTaskParamsByTask(VMF::Context& ctx) const;
         void SetTaskParamsForTask(VMF::Context& ctx, const std::string& taskName, const VMF::TaskParams& params) const;
+
+    public:
+        std::string GetSequenceName() const override;
+        VMF::DataRepositoryPtr CreateRepository() override;
+        VMF::VisionProcessorPtr CreateVisionProcessor() override;
+        VMF::SequenceBuilderPtr CreateBuilder() override;
+        void ConfigureContext(VMF::Context& ctx) override;
+       
     };
 }
 
